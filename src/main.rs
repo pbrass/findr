293a58192db7822599ca67b12f1b06165731b60b
Phil Brass <pbrass@gmail.com>
2025-06-15 22:46:44 -0400

    Got basic directory walking working I think

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+use clap::Parser;
+use walkdir::WalkDir;
+use std::fs;
+use std::path::Path;
+use std::io::Write;
+use std::io::BufRead;
+use std::io::BufReader;
+use std::io::BufWriter;
+use std::io::stdin;
+use std::io::stdout;
+use std::io::stderr;    
+
+#[derive(Debug, Parser)]
+#[command(author, version, about)]
+/// Rust version of `find`
+struct Args {
+    /// List of starting points
+    #[arg(value_name = "DIRS", default_value = ".")]
+    dirs: Vec<String>,
+}
+
+
+
+fn run(mut _args: Args) -> anyhow::Result<()> {
+    for dir in _args.dirs {
+        for entry in WalkDir::new(dir) {
+            let entry = entry?;
+            println!("{}", entry.path().display());
+        }
+    }
+    Ok(())
+}
 fn main() {
-    println!("Hello, world!");
+    if let Err(e) = run(Args::parse()) {
+        eprintln!("{e}");
+        std::process::exit(1);
+    }
 }
